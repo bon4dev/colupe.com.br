b55b93567bdb3a92d119f26f9044a7952bca900f
luelsilva <luizkrx@gmail.com>
2025-03-16 07:23:31 -0300

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Geist } from 'next/font/google';
+import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -7,10 +8,16 @@ const geistSans = Geist({
 
 export default function Home() {
   return (
-    <div
-      className={`min-h-screen flex items-center justify-center ${geistSans.variable}`}
-    >
-      <h1 className="text-4xl font-bold">LCO Systems</h1>
-    </div>
+    <>
+      <Head>
+        <title>LCO Systems</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </Head>
+      <div
+        className={`min-h-screen flex items-center justify-center ${geistSans.variable}`}
+      >
+        <h1 className="text-4xl font-bold">LCO Systems</h1>
+      </div>
+    </>
   );
 }
